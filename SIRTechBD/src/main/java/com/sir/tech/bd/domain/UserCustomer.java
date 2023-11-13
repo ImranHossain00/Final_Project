99a056aaf032c3629861025ab2b955f489044abb
ImranHossain00 <imran188566@gmail.com>
2023-11-13 15:54:08 +0600

    update commit

--- SIRTechBD/src/main/java/com/sir/tech/bd/domain/UserCustomer.java
+++ SIRTechBD/src/main/java/com/sir/tech/bd/domain/UserCustomer.java
@@ -1,11 +1,8 @@
 package com.sir.tech.bd.domain;
 
 public class UserCustomer {
-<<<<<<< HEAD
     private String username;
     private String password;
-
-=======
     private String fullName;
->>>>>>> main
+
 }
